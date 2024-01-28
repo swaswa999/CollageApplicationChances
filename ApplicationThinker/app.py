a451d23064dae387a09f5c75d601553268754c84
swaswa999 <swayams@icloud.com>
2024-01-27 16:24:31 -0800

    Basic flask app is working, will start on basic front end

---
+++ ApplicationThinker/app.py
@@ -0,0 +1,16 @@
+#main PYTHON FILE FOR FLASK
+import os
+from flask import Flask,render_template, redirect, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    host = os.environ.get('FLASK_HOST', '0.0.0.0')
+    port = int(os.environ.get('FLASK_PORT', 81))
+    app.run(host=host, port=port, debug=True)
