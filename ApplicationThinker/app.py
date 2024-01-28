040c64cab0919f4ac9973778702b1f12638eb295
swaswa999 <swayams@icloud.com>
2024-01-27 21:26:31 -0800

    created a SQL lite databace

--- ApplicationThinker/app.py
+++ ApplicationThinker/app.py
@@ -1,6 +1,42 @@
 #main PYTHON FILE FOR FLASK
 import os
 from flask import Flask,render_template, redirect, request
+import sqlite3
+
+conn = sqlite3.connect('collageapplications.db')
+
+cursor = conn.cursor()
+create_table_query = """
+CREATE TABLE IF NOT EXISTS applications (
+    fname TEXT,
+    lname TEXT,
+    age INTEGER,
+    legacy TEXT,
+    internships INTEGER,
+    competitions INTEGER,
+    leadership INTEGER,
+    state TEXT,
+    citizenship TEXT,
+    HouseholdIncome INTEGER, 
+    financialAid TEXT,
+    weightedGPA REAL,
+    Unweighted REAL, 
+    satScores INTEGER,
+    actScores INTEGER,
+    apCourses INTEGER,
+    sportsParticipation TEXT,
+    volunteerHours TEXT,
+    clubsParticipated INTEGER,
+    clubsLed INTEGER,
+    studentCouncil TEXT,
+    valedictorian INTEGER,
+    artsParticipation TEXT,
+    portfolio TEXT
+)"""
+
+cursor.execute(create_table_query)
+conn.commit()
+conn.close()
 
 
 app = Flask(__name__)
