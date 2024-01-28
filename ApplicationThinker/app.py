f71eda95445a7192e38edd88b8e83b31e94eb15c
swaswa999 <swayams@icloud.com>
2024-01-27 19:14:45 -0800

    Forgot to update, add i did was add forms and participate in the workshops

--- ApplicationThinker/app.py
+++ ApplicationThinker/app.py
@@ -13,9 +13,6 @@ def home():
 def index():
     return render_template('index.html')
 
-
-
-
 if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
     port = int(os.environ.get('FLASK_PORT', 81))
